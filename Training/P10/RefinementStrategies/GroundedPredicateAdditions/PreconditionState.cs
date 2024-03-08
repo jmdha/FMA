aa2da2f3b7fb6ab7d6d7bfaa1e3f2eea75308e13
Kristian Skov Johansen <kris701kj@gmail.com>
2024-03-08 12:29:44 +0100

    Fixes

--- Training/P10/RefinementStrategies/GroundedPredicateAdditions/PreconditionState.cs
+++ Training/P10/RefinementStrategies/GroundedPredicateAdditions/PreconditionState.cs
@@ -8,12 +8,14 @@ namespace P10.RefinementStrategies.GroundedPredicateAdditions
         public int ValidStates { get; set; }
         public int InvalidStates { get; set; }
         public int Applicability { get; set; }
+        public int InApplicability { get; set; }
         public ActionDecl MetaAction { get; set; }
         public List<IExp> Precondition { get; set; }
 
-        public PreconditionState(int validStates, int invalidStates, int applicability, ActionDecl metaAction, List<IExp> precondition)
+        public PreconditionState(int validStates, int invalidStates, int applicability, int inApplicability, ActionDecl metaAction, List<IExp> precondition)
         {
             Applicability = applicability;
+            InApplicability = inApplicability;
             ValidStates = validStates;
             InvalidStates = invalidStates;
             MetaAction = metaAction;
