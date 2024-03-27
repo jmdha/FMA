cd0fc2ed59ef2fc4ceb946372ea73c1b61fa8a42
Kristian Skov Johansen <kris701kj@gmail.com>
2024-03-27 11:48:27 +0100

    Relative fixes

--- Testing/testing.sh
+++ Testing/testing.sh
@@ -6,4 +6,4 @@ ALIAS="$3"
 DOMAIN="$4"
 PROBLEM="$5"
 
-./Dependencies/fast-downward/fast-downward.py --plan-file ${OUT} --alias ${ALIAS} ${LEARNER_DIRECTORY}/output/enhancedDomain.pddl ${PROBLEM}
+../../../../Dependencies/fast-downward/fast-downward.py --plan-file ${OUT} --alias ${ALIAS} ${LEARNER_DIRECTORY}/output/enhancedDomain.pddl ${PROBLEM}
--- Training/training.sh
+++ Training/training.sh
@@ -5,4 +5,4 @@ GENERATION_STRATEGY="$2"
 DOMAIN="$3"
 PROBLEMS="$4"
 
-../../Training/P10/bin/Release/net8.0/P10 --domain ${DOMAIN} --problems ${PROBLEMS} --generation-strategies ${GENERATION_STRATEGY} --stackelberg-path ../../Dependencies/stackelberg-planner/src/fast-downward.py --fast-downward-path ../../Dependencies/fast-downward/fast-downward.py
+../../../../Training/P10/bin/Release/net8.0/P10 --domain ${DOMAIN} --problems ${PROBLEMS} --generation-strategies ${GENERATION_STRATEGY} --stackelberg-path ../../../../Dependencies/stackelberg-planner/src/fast-downward.py --fast-downward-path ../../../../Dependencies/fast-downward/fast-downward.py
