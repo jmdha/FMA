c8838bc1b1a7de225c92576c2b8e173a93a6bb47
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-09 09:11:28 +0200

    Meta action generator setup

---
+++ Training/MetaActionCandidateGenerator/CandidateGenerators/CPDDLInvariantMetaActions.cs
@@ -0,0 +1,29 @@
+﻿using PDDLSharp.Models.PDDL.Domain;
+using PDDLSharp.Models.PDDL.Expressions;
+using PDDLSharp.Models.PDDL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDDLSharp.Models.PDDL.Overloads;
+
+namespace MetaActionCandidateGenerator.CandidateGenerators
+{
+    public class CPDDLInvariantMetaActions : BaseCandidateGenerator
+    {
+        internal override List<ActionDecl> GenerateCandidatesInner(PDDLDecl pddlDecl)
+        {
+            if (pddlDecl.Domain.Predicates == null)
+                throw new Exception("No predicates defined in domain!");
+            Initialize(pddlDecl);
+            ContextualizeIfNotAlready(pddlDecl);
+
+            var candidates = new List<ActionDecl>();
+
+
+            return candidates.Distinct(pddlDecl.Domain.Actions);
+
+        }
+    }
+}
