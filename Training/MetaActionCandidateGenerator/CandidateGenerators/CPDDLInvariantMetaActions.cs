06db0f97d60248d57c24c10d2ce621261d4392c8
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-11 10:25:14 +0200

    Got the negated variant of the CPDDL meta actions to work closes #58

--- Training/MetaActionCandidateGenerator/CandidateGenerators/CPDDLInvariantMetaActions.cs
+++ Training/MetaActionCandidateGenerator/CandidateGenerators/CPDDLInvariantMetaActions.cs
@@ -1,11 +1,19 @@
-﻿using PDDLSharp.CodeGenerators.PDDL;
+﻿using CommandLine.Text;
+using Microsoft.VisualBasic.FileIO;
+using PDDLSharp.CodeGenerators.PDDL;
 using PDDLSharp.ErrorListeners;
 using PDDLSharp.Models.PDDL;
 using PDDLSharp.Models.PDDL.Domain;
 using PDDLSharp.Models.PDDL.Expressions;
 using PDDLSharp.Models.PDDL.Overloads;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Tools;
+using static System.Collections.Specialized.BitVector32;
 
 namespace MetaActionCandidateGenerator.CandidateGenerators
 {
@@ -30,128 +38,29 @@ namespace MetaActionCandidateGenerator.CandidateGenerators
             {
                 if (!Statics.Any(x => x.Name.ToUpper() == predicate.Name.ToUpper()))
                 {
-                    int counter = 0;
                     foreach (var action in pddlDecl.Domain.Actions)
                     {
                         if (action.Effects.FindNames(predicate.Name).Count == 0)
                             continue;
 
-                        // Possitives
-                        if (!predicate.CanOnlyBeSetToFalse(pddlDecl.Domain))
+                        bool invarianted = rules.Any(x => x.Any(y => y.Predicate == predicate.Name));
+                        if (invarianted)
+                            candidates.AddRange(GeneateCandidates(rules, pddlDecl, predicate, action));
+                        else
                         {
-                            var handled = new List<string>();
-                            var preconditions = new List<IExp>();
-                            var effects = new List<IExp>() { predicate };
-                            var singles = rules.Where(x => x.Count == 1 && x[0].Predicate == predicate.Name).ToList();
-
-                            foreach(var single in singles)
-                            {
-                                var newArgs = new List<NameExp>();
-                                int offset = 0;
-                                foreach(var arg in single[0].Args)
-                                {
-                                    if (arg.StartsWith('V'))
-                                        newArgs.Add(predicate.Arguments[offset].Copy());
-                                    else if (arg.StartsWith('C'))
-                                        newArgs.Add(new NameExp($"{predicate.Arguments[offset].Name}{offset}", predicate.Arguments[offset].Type.Copy()));
-                                    offset++;
-                                }
-
-                                var newPredicate = new PredicateExp(single[0].Predicate, newArgs);
-
-                                if (!newPredicate.CanOnlyBeSetToFalse(pddlDecl.Domain) &&
-                                    !newPredicate.CanOnlyBeSetToTrue(pddlDecl.Domain))
-                                {
-                                    preconditions.Add(newPredicate);
-                                    var negated = new NotExp(newPredicate.Copy());
-                                    negated.Child.Parent = negated;
-                                    effects.Add(negated);
-                                    handled.Add(single[0].Predicate);
-                                }
-                            }
-                            var complexes = rules.Where(x => x.Count > 1 && x.Any(y => y.Predicate == predicate.Name)).ToList();
-                            if (complexes.Count == 0)
-                            {
+                            if (!predicate.CanOnlyBeSetToFalse(pddlDecl.Domain))
                                 candidates.Add(GenerateMetaAction(
-                                    $"meta_{predicate.Name}_{counter++}",
-                                    preconditions,
-                                    effects,
+                                    $"meta_{predicate.Name}",
+                                    new List<IExp>(),
+                                    new List<IExp>() { predicate },
                                     action));
-                            }
-                            else
-                            {
-                                foreach (var complex in complexes)
-                                {
-                                    var match = complex.First(x => x.Predicate == predicate.Name);
-                                    foreach (var option in complex)
-                                    {
-                                        if (option.Predicate == predicate.Name)
-                                            continue;
-
-                                        var newPreconditions = new List<IExp>(preconditions);
-                                        var newEffects = new List<IExp>(effects);
-
-                                        var sample = pddlDecl.Domain.Predicates.Predicates.First(x => x.Name == option.Predicate);
-                                        if (!sample.CanOnlyBeSetToFalse(pddlDecl.Domain) &&
-                                            !sample.CanOnlyBeSetToTrue(pddlDecl.Domain))
-                                        {
-                                            foreach (var eff in effects)
-                                            {
-                                                if (eff is NotExp not && not.Child is PredicateExp pred && pred.Name == match.Predicate)
-                                                {
-                                                    var newArgs = new List<NameExp>();
-                                                    int index = 0;
-                                                    foreach (var arg in option.Args)
-                                                    {
-                                                        var targetIndex = match.Args.IndexOf(arg);
-                                                        if (targetIndex != -1)
-                                                            newArgs.Add(pred.Arguments[targetIndex].Copy());
-                                                        else
-                                                             newArgs.Add(new NameExp($"?{arg}", sample.Arguments[index].Type.Copy()));
-                                                        index++;
-                                                    }
-                                                    var newPredicate = new PredicateExp(option.Predicate, newArgs);
-                                                    newEffects.Add(newPredicate);
-                                                }
-                                                else if (eff is PredicateExp pred2 && pred2.Name == match.Predicate)
-                                                {
-                                                    var newArgs = new List<NameExp>();
-                                                    int index = 0;
-                                                    foreach (var arg in option.Args)
-                                                    {
-                                                        var targetIndex = match.Args.IndexOf(arg);
-                                                        if (targetIndex != -1)
-                                                            newArgs.Add(pred2.Arguments[targetIndex].Copy());
-                                                        else
-                                                            newArgs.Add(new NameExp($"?{arg}", sample.Arguments[index].Type.Copy()));
-                                                        index++;
-                                                    }
-                                                    var newPredicate = new PredicateExp(option.Predicate, newArgs);
-                                                    newPreconditions.Add(newPredicate);
-                                                    newEffects.Add(new NotExp(newPredicate));
-                                                }
-                                            }
-                                        }
-
-                                        if (newPreconditions.Count != preconditions.Count &&
-                                            newEffects.Count != effects.Count)
-                                                candidates.Add(GenerateMetaAction(
-                                                    $"meta_{predicate.Name}_{counter++}",
-                                                    newPreconditions,
-                                                    newEffects,
-                                                    action));
-                                    }
-                                }
-                            }
+                            if (!predicate.CanOnlyBeSetToTrue(pddlDecl.Domain))
+                                candidates.Add(GenerateMetaAction(
+                                    $"meta_{predicate.Name}_false",
+                                    new List<IExp>(),
+                                    new List<IExp>() { new NotExp(predicate) },
+                                    action));
                         }
-
-                        // Negatives (TODO)
-                        if (!predicate.CanOnlyBeSetToTrue(pddlDecl.Domain))
-                            candidates.Add(GenerateMetaAction(
-                                $"meta_{predicate.Name}_{counter++}_false",
-                                new List<IExp>(),
-                                new List<IExp>() { new NotExp(predicate) },
-                                action));
                     }
                 }
             }
@@ -182,11 +91,14 @@ namespace MetaActionCandidateGenerator.CandidateGenerators
             process.Start();
             process.WaitForExit();
 
+            if (!File.Exists(Path.Combine(TempFolder, "output.txt")))
+                return new List<List<PredicateRule>>();
+
             var rules = new List<List<PredicateRule>>();
 
             foreach (var line in File.ReadLines(Path.Combine(TempFolder, "output.txt")))
             {
-                if (line.Contains(":=1"))
+                if (line.EndsWith(":=1"))
                 {
                     var inner = line.Substring(line.IndexOf('{') + 1, line.IndexOf('}') - line.IndexOf('{') - 1);
                     var subRules = new List<PredicateRule>();
@@ -224,6 +136,128 @@ namespace MetaActionCandidateGenerator.CandidateGenerators
             }
 
             return rules;
+        }
+
+        private List<ActionDecl> GeneateCandidates(List<List<PredicateRule>> rules, PDDLDecl pddlDecl, PredicateExp predicate, ActionDecl staticsReference)
+        {
+            var candidates = new List<ActionDecl>();
+            var preconditions = new List<IExp>();
+            var effects = new List<IExp>() { predicate };
+
+            // Singles
+            InsertSinglesFromRules(rules, pddlDecl, predicate, ref preconditions, ref effects);
+
+            // Complexes
+            var complexes = rules.Where(x => x.Count > 1 && x.Any(y => y.Predicate == predicate.Name)).ToList();
+            if (complexes.Count == 0)
+            {
+                candidates.Add(GenerateMetaAction(
+                    $"meta_{predicate.Name}",
+                    preconditions,
+                    effects,
+                    staticsReference));
+            }
+            else
+            {
+                foreach (var complex in complexes)
+                {
+                    var match = complex.First(x => x.Predicate == predicate.Name);
+                    foreach (var option in complex)
+                    {
+                        if (option.Predicate == predicate.Name)
+                            continue;
+
+                        var newPreconditions = new List<IExp>(preconditions);
+                        var newEffects = new List<IExp>(effects);
+
+                        var sample = pddlDecl.Domain.Predicates!.Predicates.First(x => x.Name == option.Predicate);
+                        if (!sample.CanOnlyBeSetToFalse(pddlDecl.Domain) &&
+                            !sample.CanOnlyBeSetToTrue(pddlDecl.Domain))
+                        {
+                            foreach (var eff in effects)
+                            {
+                                if (eff is NotExp not && not.Child is PredicateExp pred && pred.Name == match.Predicate)
+                                    UpdateByDirection(GenerateMutated(option, match, sample, pred), ref newPreconditions, ref newEffects, false);
+                                else if (eff is PredicateExp pred2 && pred2.Name == match.Predicate)
+                                    UpdateByDirection(GenerateMutated(option, match, sample, pred2), ref newPreconditions, ref newEffects, true);
+                            }
+                        }
+
+                        if (newPreconditions.Count != preconditions.Count &&
+                            newEffects.Count != effects.Count)
+                            candidates.Add(GenerateMetaAction(
+                                $"meta_{predicate.Name}",
+                                newPreconditions,
+                                newEffects,
+                                staticsReference));
+                    }
+                }
+            }
+            return candidates;
+        }
+
+        private void InsertSinglesFromRules(List<List<PredicateRule>> rules, PDDLDecl pddlDecl, PredicateExp predicate, ref List<IExp> preconditions, ref List<IExp> effects)
+        {
+            var singles = rules.Where(x => x.Count == 1 && x[0].Predicate == predicate.Name).ToList();
+
+            foreach (var single in singles)
+            {
+                var newArgs = new List<NameExp>();
+                int offset = 0;
+                foreach (var arg in single[0].Args)
+                {
+                    if (arg.StartsWith('V'))
+                        newArgs.Add(predicate.Arguments[offset].Copy());
+                    else if (arg.StartsWith('C'))
+                        newArgs.Add(new NameExp($"{predicate.Arguments[offset].Name}{offset}", predicate.Arguments[offset].Type.Copy()));
+                    offset++;
+                }
+
+                var newPredicate = new PredicateExp(single[0].Predicate, newArgs);
+
+                if (!newPredicate.CanOnlyBeSetToFalse(pddlDecl.Domain) &&
+                    !newPredicate.CanOnlyBeSetToTrue(pddlDecl.Domain))
+                {
+                    UpdateByDirection(newPredicate, ref preconditions, ref effects, true);
+                }
+            }
+        }
+
+        private void UpdateByDirection(PredicateExp predicate, ref List<IExp> preconditions, ref List<IExp> effects, bool direction)
+        {
+            if (direction)
+            {
+                preconditions.Add(predicate);
+                effects.Add(GenerateNegated(predicate.Copy()));
+            }
+            else
+            {
+                //preconditions.Add(GenerateNegated(predicate.Copy()));
+                effects.Add(predicate);
+            }
+        }
+
+        private NotExp GenerateNegated(IExp exp)
+        {
+            var newNot = new NotExp(exp);
+            newNot.Child.Parent = newNot;
+            return newNot;
+        }
+
+        private PredicateExp GenerateMutated(PredicateRule option, PredicateRule match, PredicateExp sample, PredicateExp predicate)
+        {
+            var newArgs = new List<NameExp>();
+            int index = 0;
+            foreach (var arg in option.Args)
+            {
+                var targetIndex = match.Args.IndexOf(arg);
+                if (targetIndex != -1)
+                    newArgs.Add(predicate.Arguments[targetIndex].Copy());
+                else
+                    newArgs.Add(new NameExp($"?{arg}", sample.Arguments[index].Type.Copy()));
+                index++;
+            }
+            return new PredicateExp(option.Predicate, newArgs);
         }
 
         private class PredicateRule
