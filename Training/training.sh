fb09390da65b1e40682f713341abeb63b705e22d
Kristian Skov Johansen <kris701kj@gmail.com>
2024-04-16 10:44:55 +0200

    Added a training only experiment run

--- Training/training.sh
+++ Training/training.sh
@@ -6,4 +6,4 @@ USEFULNESS_STRATEGY_POST="$3"
 DOMAIN="$4"
 PROBLEMS="${@:5}"
 
-../../../../Training/P10/bin/Release/net8.0/P10 --domain ${DOMAIN} --problems ${PROBLEMS} --generation-strategies ${GENERATION_STRATEGY} --stackelberg-path ../../../../Dependencies/stackelberg-planner/src/fast-downward.py --fast-downward-path ../../../../Dependencies/fast-downward/fast-downward.py  --cpddl-path ../../../../Dependencies/cpddl/bin/pddl --pre-usefulness-strategy UsedInPlans --post-usefulness-strategy ${USEFULNESS_STRATEGY_POST}
+../../../../Training/P10/bin/Release/net8.0/P10 --domain ${DOMAIN} --problems ${PROBLEMS} --generation-strategies ${GENERATION_STRATEGY} --stackelberg-path ../../../../Dependencies/stackelberg-planner/src/fast-downward.py --fast-downward-path ../../../../Dependencies/fast-downward/fast-downward.py  --cpddl-path ../../../../Dependencies/cpddl/bin/pddl --pre-usefulness-strategy UsedInPlans --post-usefulness-strategy ${USEFULNESS_STRATEGY_POST} --validation-time-limit 120 --exploration-time-limit 120 --refinement-time-limit 240
