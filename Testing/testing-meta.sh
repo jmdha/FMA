dd6e18fd744ee91d1b311ef8154c857ee83e83da
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-05-10 05:33:49 +0200

    misc: update suites to new format

--- Testing/testing-meta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OUT="$1"
-ALIAS="$2"
-LEARNER_DIRECTORY="$3"
-DOMAIN="$4"
-PROBLEM="$5"
-
-../../../../Dependencies/fast-downward/fast-downward.py --plan-file ${OUT} --alias ${ALIAS} ${LEARNER_DIRECTORY}/output/enhancedDomain.pddl ${PROBLEM}
