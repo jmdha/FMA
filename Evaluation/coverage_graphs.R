ebd586bc23d3881374968c30f29c5348d612ac11
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-13 09:53:03 +0200

    Added coverage graphs

--- Evaluation/coverage_graphs.R
+++ Evaluation/coverage_graphs.R
@@ -6,9 +6,9 @@ source("Tools/graphNames.R")
 
 # Handle arguments
 args = commandArgs(trailingOnly=TRUE)
-args[1] <- "solve.csv"
-args[2] <- "S_CPDDL"
-args[3] <- "LAMA_FIRST"
+#args[1] <- "solve.csv"
+#args[2] <- "S_CPDDL"
+#args[3] <- "LAMA_FIRST"
 if (length(args) < 1) {
   stop("At least 1 argument must be supplied! The source data file, and one for each target reconstruction type", call.=FALSE)
 }
@@ -33,3 +33,28 @@ data <- read.csv(
 data <- rename_data(data)
 
 solved <- data[data$exit_code == 0,]
+
+result <- data.frame(Domain=character(), P=integer())
+for(target in targets)
+  result[target] <- integer()
+
+for(domain in unique(data$domain))
+{
+  newRow <- c(domain,length(unique(data[data$domain == domain,]$problem)))
+  
+  for(target in targets){
+    this <- solved[solved$name == target,]
+    this <- this[this$domain == domain,]
+    newRow <- c(newRow, length(this$problem))
+  }
+  
+  result[nrow(result) + 1,] = newRow
+}
+
+generate_table(
+  result,
+  paste("out/coverage.tex", sep = ""),
+  colnames(result),
+  "\\textit{Coverage of how many problems each method was able to solve within the time limit.}",
+  "tab:coverage"
+)
--- Evaluation/runall.sh
+++ Evaluation/runall.sh
@@ -4,3 +4,5 @@ echo "== Refinement Graphs =="
 Rscript refinement_graphs.R refinement.csv CPDDLMutexed
 echo "== Testing Graphs =="
 Rscript testing_graphs.R solve.csv S_CPDDL LAMA_FIRST
+echo "== Coverage Graphs =="
+Rscript coverage_graphs.R solve.csv LAMA_FIRST S_CPDDL
