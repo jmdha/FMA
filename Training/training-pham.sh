36cc88c008ec91d7b0bf9e952283d1dabf56be90
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2024-05-21 16:02:41 +0200

    split pham

--- Training/training-pham.sh
+++ Training/training-pham.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 OUT="$1"
-GENERATION_STRATEGY="$2"
 DOMAINNAME="$3"
 DOMAIN="$4"
 PROBLEMS="${@:5}"
 
-../../../../Training/FocusedMetaActions.Train/bin/Release/net8.0/FocusedMetaActions.Train --domain ${DOMAIN} --problems ${PROBLEMS} --generator ${GENERATION_STRATEGY} --args metaPath\;../../../../Dependencies/focused-meta-actions-benchmarks/Pham-Domains/${DOMAINNAME}/ --stackelberg-path ../../../../Dependencies/modified-stackelberg-planner/src/fast-downward.py --fast-downward-path ../../../../Dependencies/fast-downward/fast-downward.py --validation-time-limit 120 --exploration-time-limit 240 --refinement-time-limit 480 --cache-generation-time-limit 240 --usefulness-time-limit 60 --skip-refinement
+../../../../Training/FocusedMetaActions.Train/bin/Release/net8.0/FocusedMetaActions.Train --domain ${DOMAIN} --problems ${PROBLEMS} --generator Manual --args metaPath\;../../../../Dependencies/focused-meta-actions-benchmarks/Pham-Domains/${DOMAINNAME}/ --stackelberg-path ../../../../Dependencies/modified-stackelberg-planner/src/fast-downward.py --fast-downward-path ../../../../Dependencies/fast-downward/fast-downward.py --validation-time-limit 120 --exploration-time-limit 240 --refinement-time-limit 480 --cache-generation-time-limit 240 --usefulness-time-limit 60 --skip-refinement
