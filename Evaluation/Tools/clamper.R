8cf17a1475ee60033478a9178c56eb5482bf3507
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-24 10:22:20 +0200

    Graph updates

--- Evaluation/Tools/clamper.R
+++ Evaluation/Tools/clamper.R
@@ -9,3 +9,10 @@ min_unsolved <- function(data, target) {
 	data[is.na(data[,target]),][,target] <- lowest
 	return (data)
 }
+
+max_unsolved_two <- function(data, target1, target2) {
+  highest <- max(data[,target1], data[,target2], na.rm=TRUE)
+  data[is.na(data[,target1]),][,target1] <- highest
+  data[is.na(data[,target2]),][,target2] <- highest
+  return (data)
+}
