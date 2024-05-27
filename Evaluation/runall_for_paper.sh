fb001f7fa1807fcc524f7d979584fe47e72a99b0
Kristian Skov Johansen <kris701kj@gmail.com>
2024-05-27 12:24:00 +0200

    Added a script to generate all the graphs used in the paper

---
+++ Evaluation/runall_for_paper.sh
@@ -0,0 +1,4 @@
+Rscript general_graphs.R Results/general.csv Results/mutexgroups.csv CPDDLMutexed+UsedInPlans+ReducesPlanLengthTop2
+Rscript coverage_graphs.R Results/solve.csv CPDDL LAMA_FIRST 
+Rscript testing_reconstruction_graphs.R Results/solve.csv CPDDL LAMA_FIRST
+Rscript testing_reconstruction_graphs.R Results/solve.csv CPDDL PHAM
